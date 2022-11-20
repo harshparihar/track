0fe3e01324daab3715deee5c8efff36e37cf77c4
Harshvardhan Parihar <harshvardhanparihar@gmail.com>
2022-11-20 14:21:07 +0530

    Removed Goal root

--- app/views/goals/_goal.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! goal, :id, :name, :created_at, :updated_at
-json.url goal_url(goal, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_20_082910) do
   enable_extension "plpgsql"
 
   create_table "goals", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
